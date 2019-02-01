9e46408548e9adbb2d1c03ed8ed7027be5f8bcc6
Justin Chen <jchen06@stuy.edu>
2019-02-01 02:07:05 -0500

    Created Recursion class, added empty methods & descriptions

---
+++ Recursion.java
@@ -0,0 +1,28 @@
+public class Recursion {
+
+  /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+
+    */
+    public static double sqrt(double n, double tolerance){
+
+    }
+
+    /*Recursively find the n'th fibbonaci number in linear time
+    *fib(0) = 1; fib(1) = 1; fib(5) = 5
+    *precondition: n is non-negative
+    */
+   public static int fib(int n){
+
+   }
+
+   /*Recursively finds and returns all the subset totals of the numbers
+   *1 to n inclusive in an ArrayList of integers
+   *precondition: n is non-negative
+   */
+   public static ArrayList<Integer> makeAllSums(){
+     
+   }
+
+}
